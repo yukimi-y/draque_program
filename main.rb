24d70257223582404f4bba07a47037904798b1f6
yukimi.y <yyy.yukimi@gmail.com>
2020-12-18 20:18:31 +0900

    monster.hpが0の時攻撃しないよう修正

--- main.rb
+++ main.rb
@@ -2,15 +2,15 @@ require "./brave.rb"
 require "./monster.rb"
 
 brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
-monster = Monster.new(name: "アークデーモン", hp: 210, offense: 140, defense: 80)
+monster = Monster.new(name: "アークデーモン", hp: 150, offense: 140, defense: 80)
 
 puts "#{monster.name}があらわれた！"
 
 
-while brave.hp > 0 && monster.hp > 0
+while brave.hp > 0 && monster.hp > 0 do
   brave.attack(monster)
-  monster.attack(brave)
-
+  monster.attack(brave) if monster.hp > 0
+  
   puts <<~TEXT
   *=*=*=*=*=*=*=*=*=*=**=*=*=*=*=*=*=*
   【#{brave.name}】HP: #{brave.hp}
@@ -18,11 +18,10 @@ while brave.hp > 0 && monster.hp > 0
   *=*=*=*=*=*=*=*=*=*=**=*=*=*=*=*=*=*
   TEXT
 
-  if brave.hp > 0
-    puts "#{monster.name}をやっつけた！"
-  else
-    puts "#{brave.name}はしんでしまった"
-  end
 end
 
-
+if brave.hp > 0
+  puts "#{monster.name}をやっつけた！"
+else
+  puts "#{brave.name}はしんでしまった"
+end
